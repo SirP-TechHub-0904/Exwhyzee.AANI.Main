2f09480357922d8661769faebe4015a685052a9f
VS-GitHub-001 <105536514+VS-GitHub-001@users.noreply.github.com>
2024-06-28 14:03:04 +0100

    as

---
+++ Exwhyzee.AANI.Web/Areas/Main/Pages/ParticipantPage/Reconcile.cshtml.cs
@@ -0,0 +1,46 @@
+using Exwhyzee.AANI.Domain.Enums;
+using Exwhyzee.AANI.Domain.Models;
+using Exwhyzee.AANI.Web.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace Exwhyzee.AANI.Web.Areas.Main.Pages.ParticipantPage
+{
+    //public class ReconcileModel : PageModel
+    public class ReconcileModel : PageModel
+    {
+        private readonly UserManager<Participant> _userManager;
+        private readonly Exwhyzee.AANI.Web.Data.AaniDbContext _context;
+
+        public ReconcileModel(UserManager<Participant> userManager, AaniDbContext context)
+        {
+            _userManager = userManager;
+            _context = context;
+        }
+
+        public IQueryable<Participant>? Participants { get; set; }
+         
+        public async Task<IActionResult> OnGetAsync(AliveStatus aliveStatus = 0, GenderStatus genderStatus = 0, ActiveStatus activeStatus = 0, VerificationStatus verificationStatus = 0, long chapterid = 0, long secid = 0)
+        {
+            Participants = _userManager.Users.Include(x => x.SEC).Where(x => x.Email != "jinmcever@gmail.com").AsQueryable();
+            var DataParticipants = await _userManager.Users.AsNoTracking().Include(x => x.SEC).Where(x => x.Email.Contains("@aani") && x.EmergencyContactEmail != null).ToListAsync();
+            int cu = DataParticipants.Count();
+            foreach(var x in DataParticipants)
+            {
+                var user = await _userManager.FindByIdAsync(x.Id);
+                user.Email = x.EmergencyContactEmail;
+                await _userManager.UpdateAsync(user);
+            }
+
+            
+            //await Task.Run();
+            TempData["data"] = "ALL MEMBERS";
+
+            return Page();
+        }
+
+    }
+
+}
