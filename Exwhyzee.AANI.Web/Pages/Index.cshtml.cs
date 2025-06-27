11a66bcde660b0f717d682cf034451d96a2d779e
VS-GitHub-001 <105536514+VS-GitHub-001@users.noreply.github.com>
2025-06-27 16:34:42 +0100

    n

--- Exwhyzee.AANI.Web/Pages/Index.cshtml.cs
+++ Exwhyzee.AANI.Web/Pages/Index.cshtml.cs
@@ -1,8 +1,7 @@
 ﻿using Exwhyzee.AANI.Domain.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using System.Data.Entity;
-
+using Microsoft.EntityFrameworkCore;
 namespace Exwhyzee.AANI.Web.Pages
 {
     public class IndexModel : PageModel
@@ -19,13 +18,16 @@ namespace Exwhyzee.AANI.Web.Pages
         public IList<Slider> Sliders { get; set; }
         public IList<Blog> Blogs { get; set; }
         public IList<ExecutivePosition> ExecutivePositions { get; set; }
+        public IList<Executive> Executive { get; set; }
 
         public async Task OnGetAsync()
         {
             Sliders = _context.Sliders.OrderBy(d => d.SortOrder).ToList();
             Blogs = _context.Blogs.OrderByDescending(d => d.Date).Take(5).ToList();
             ExecutivePositions = _context.ExecutivePositions.Include(x => x.Campains).ToList();
-
+            Executive = await _context.Executives
+                .Include(e => e.ExecutivePosition)
+                .Include(e => e.Participant).ToListAsync();
         }
     }
 }
