c204499c7c77838033501c8abcd7af51090981fb
VS-GitHub-001 <105536514+VS-GitHub-001@users.noreply.github.com>
2025-09-22 11:20:10 +0100

    patches

--- Exwhyzee.AANI.Web/Program.cs
+++ Exwhyzee.AANI.Web/Program.cs
@@ -12,7 +12,7 @@ using System.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container.//
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<AaniDbContext>(options =>
     options.UseSqlServer(connectionString));
